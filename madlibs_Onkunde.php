0d0a736645473265822c42fc6816ee38a59ce773
Bailey van Antwerpen <99041848@mydavinci.nl>
2017-02-23 13:24:10 +0100

    Create git

---
+++ madlibs_Onkunde.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Madlibs</title>
+</head>
+<body>
+<button>Onkunde</button>
+<button>Er is paniek...</button>
+<h1>Onkunde</h1>
+<p>Er zijn veel mensen die niet kunnen <?php echo $_POST["AA"]//AA?>. Neem nou <?php echo $_POST["AB"]//AB?>. Zelfs met de hulp van een <?php echo $_POST["AD"]//AD?> of zelfs <?php echo $_POST["AC"]//AC?> kan <?php echo $_POST["AB"]//AB?> niet <?php echo $_POST["AA"]//AA?>. Dat heeft niet te maken met een gebrek aan <?php echo $_POST["AE"]//AE?>, maar met een te veel aan <?php echo $_POST["AF"]//AF?>. Te veel <?php echo $_POST["AF"]//AF?> leidt tot <?php echo $_POST["AG"]//AG?> en dat is niet goed als je wilt <?php echo $_POST["AA"]//AA?>. Helaas voor <?php echo $_POST["AB"]//AB?>.</p>
+</body>
+</html>
