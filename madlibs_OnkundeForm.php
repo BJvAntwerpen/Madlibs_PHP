728da0464a525c34fc309d16d4118a6e1e917f45
Bailey van Antwerpen <99041848@mydavinci.nl>
2017-03-07 11:19:26 +0100

    update text

--- madlibs_OnkundeForm.php
+++ madlibs_OnkundeForm.php
@@ -8,6 +8,11 @@
 <button id="buttonOnkunde"><a href="Index.php">Onkunde</a></button>
 <button id="buttonPaniek"><a href="madlibs_Paniek.php">Er is paniek...</a></button>
 <h1>Onkunde</h1>
-<p>Er zijn veel mensen die niet kunnen <?php echo $_POST["AA"]//AA?>. Neem nou <?php echo $_POST["AB"]//AB?>. Zelfs met de hulp van een <?php echo $_POST["AD"]//AD?> of zelfs <?php echo $_POST["AC"]//AC?> kan <?php echo $_POST["AB"]//AB?> niet <?php echo $_POST["AA"]//AA?>. Dat heeft niet te maken met een gebrek aan <?php echo $_POST["AE"]//AE?>, maar met een te veel aan <?php echo $_POST["AF"]//AF?>. Te veel <?php echo $_POST["AF"]//AF?> leidt tot <?php echo $_POST["AG"]//AG?> en dat is niet goed als je wilt <?php echo $_POST["AA"]//AA?>. Helaas voor <?php echo $_POST["AB"]//AB?>.</p>
+<p>Er zijn veel mensen die niet kunnen <?php echo $_POST["AA"]//AA?>.<br>
+Neem nou <?php echo $_POST["AB"]//AB?>.<br>
+Zelfs met de hulp van een <?php echo $_POST["AD"]//AD?> of zelfs <?php echo $_POST["AC"]//AC?> kan <?php echo $_POST["AB"]//AB?> niet <?php echo $_POST["AA"]//AA?>.<br>
+Dat heeft niet te maken met een gebrek aan <?php echo $_POST["AE"]//AE?>, maar met een te veel aan <?php echo $_POST["AF"]//AF?>.<br>
+Te veel <?php echo $_POST["AF"]//AF?> leidt tot <?php echo $_POST["AG"]//AG?> en dat is niet goed als je wilt <?php echo $_POST["AA"]//AA?>.<br>
+Helaas voor <?php echo $_POST["AB"]//AB?>.</p>
 </body>
 </html>
--- madlibs_PaniekForm.php
+++ madlibs_PaniekForm.php
@@ -11,7 +11,7 @@
 <p>Er heerst paniek in het koninkrijk <?php echo $_POST["BC"]//BC?>. Koning <?php echo $_POST["BF"]//BF?> is ten einde raad en als koning <?php echo $_POST["BF"]//BF?> ten einde raad is, dan roept hij zijn ten-einde-raadsheer <?php echo $_POST["BB"]//BB?>.<br>
 "<?php echo $_POST["BB"]//BB?>! Het is een ramp! Het is een schande!"<br>
 "Sire, Majesteit, Uwe Luidruchtigheid, wat is er aan de hand?"<br>
-"Mijn <?php echo $_POST["BA"]//BA?> is verdwenen! Zo maar, zonder waarschuwing. En ik had net <?php echo $_POST["BE"]//BE?> vor hem gekocht!"<br>
+"Mijn <?php echo $_POST["BA"]//BA?> is verdwenen! Zo maar, zonder waarschuwing. En ik had net <?php echo $_POST["BE"]//BE?> voor hem gekocht!"<br>
 "Majesteit, uw <?php echo $_POST["BA"]//BA?> komt vast vanzelf weer terug?"<br>
 "Ja, da's leuk en aardig, maar hoe moet ik in de tussentijd <?php echo $_POST["BH"]//BH?> leren?"<br>
 "Maar Sire, daar kunt u toch uw <?php echo $_POST["BG"]//BG?> voor gebruiken."<br>
